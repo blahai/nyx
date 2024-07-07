9f6e5239b8fbc99569d7b7e11dc80f911fb97e64
blahai <github@blahai.gay>
2024-07-07 03:08:00 +0300

    nvm

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0-rc1.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -26,14 +22,13 @@
     nurpkgs.url = "github:nix-community/NUR";
   };
 
-  outputs = inputs@{ self, nixpkgs, lix-module, nur, ... }: {
+  outputs = inputs@{ self, nixpkgs, nur, ... }: {
     nixosConfigurations = {
 
       nyx = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/nyx/configuration.nix
-          lix-module.nixosModules.default
           inputs.home-manager.nixosModules.home-manager
           {
             nixpkgs.overlays = [ nur.overlay ];
