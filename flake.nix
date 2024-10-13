c5492b9c4665a79f430dee7dabf6f2ce0c943bcd
blahai <github@blahai.gay>
2024-10-13 20:21:28 +0300

    add unfree cache cuz otherwise davinci takes 9 years to do shit

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
+      "https://nixpkgs-unfree.cachix.org"
       "https://hyprland.cachix.org/"
       "https://anyrun.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
     ];
