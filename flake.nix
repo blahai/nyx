f9314f93b3397d3233f98528a430c74804504339
blahai <github@blahai.gay>
2024-10-30 00:01:43 +0200

    Flake: add hydra as substituter. Nyx: add trusted substituters to nix config

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
       "https://hyprland.cachix.org/"
       "https://anyrun.cachix.org"
       "https://wezterm.cachix.org"
+      "https://hydra.nixos.org/" 
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
@@ -15,6 +16,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
+      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs=" 
     ];
 
     auto-optimise-store = true;
--- hosts/nyx/configuration.nix
+++ hosts/nyx/configuration.nix
@@ -177,6 +177,22 @@
       warn-dirty = false;
       use-xdg-base-directories = true;
       trusted-users = [ "@wheel" "pingu" "root" ];
+      trusted-substituters = [
+        "https://nix-community.cachix.org"
+        "https://nixpkgs-unfree.cachix.org"
+        "https://hyprland.cachix.org/"
+        "https://anyrun.cachix.org"
+        "https://wezterm.cachix.org"
+        "https://hydra.nixos.org/" 
+      ];
+      trusted-public-keys = [ 
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+        "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
+        "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs=" 
+      ];
     };
   };
 
