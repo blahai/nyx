8474eaeec6f6845c6fc69cf04c70d79416ff91de
blahai <github@blahai.gay>
2024-10-31 14:29:22 +0200

    Nyx: prepare for zfs (need to wait for 6.12 so I can stay on lts)

--- hosts/nyx/configuration.nix
+++ hosts/nyx/configuration.nix
@@ -19,6 +19,11 @@
       };
       efi.canTouchEfiVariables = true;
     };
+    # supportedFilesystems = [ "zfs" ];
+    # zfs = {
+    #   forceImportRoot = false;
+    #   package = pkgs.zfs_unstable;
+    # };
     kernelPackages = pkgs.linuxPackages_zen;
     kernel = {
       sysctl ={
@@ -35,6 +40,7 @@
 
   networking = {
     hostName = "nyx";
+    hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
     networkmanager.enable = true;
     stevenblack = {
       enable = true;
